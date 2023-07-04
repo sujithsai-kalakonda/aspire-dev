cae17c80e403d660db090a94b9fd1159c1d195e4
sujithsai-kalakonda <108214819+sujithsai-kalakonda@users.noreply.github.com>
2023-07-05 00:01:47 +0530

    Add verifyForgotPassword.js route file

---
+++ backend/routes/verifyForgotPassword.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../users');
+const { generateOTP, sendOTPByEmail } = require('../utils');
+
+// Handle the POST request to verify the OTP for forgot password
+router.post('/verifyforgotpassword', async (req, res) => {
+  const { usernameOrEmail, otp } = req.body;
+
+  try {
+    // Find the user by username or email
+    const user = await User.findOne({
+      $or: [{ username: usernameOrEmail }, { email: usernameOrEmail }],
+    });
+
+    if (!user) {
+      return res.status(400).json({ error: 'User not found' });
+    }
+
+    // Check if the OTP matches the user's OTP
+    if (otp === user.otp) {
+      // Update the isVerified_forgot property to true
+      user.isVerified_forgot = true;
+      await user.save();
+
+      // Return a success response
+      return res.json({ message: 'OTP verification successful' });
+    }
+
+    return res.status(400).json({ error: 'Invalid OTP' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+module.exports = router;
